5dd401bad8ef6cf4562efc7d128c6b42292626dd
João Felipe da rosa <80059048+JoaoFelipe-rosa@users.noreply.github.com>
2021-07-15 21:41:44 -0300

    Rocket.Q-NLW- 3.2

---
+++ public/scripts/main.js
@@ -0,0 +1,13 @@
+import Modal from './modal.js'
+
+const modal = Modal()
+//pegar todos os botões que existem com a classe check
+const checkButtons = document.querySelectorAll(".action a.check")
+
+checkButtons.forEach(button => {
+    //adicionar escuta
+    button.addEventListener("click", event => {
+        //abrir modal
+        modal.open
+    })
+})
