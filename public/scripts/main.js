d985bc50a455437a454032fe9d50f439ff67075f
João Felipe da rosa <80059048+JoaoFelipe-rosa@users.noreply.github.com>
2021-07-27 13:34:13 -0300

    Rocket.Q-NLW- 3.6

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -23,9 +23,12 @@ function handleClick(event, check = true) {
     event.preventDefault()
 
     const text = check ? "Marcar como lida" : "Excluir"
+    const slug = check ? "check" : "delete"
+    const roomId = document.querySelector("#room-id").dataset.id
+    const questionId = event.target.dataset.id
 
     const form = document.querySelector(".modal form")
-    form.setAttribute("action", `/room/:room/:question/:action`)
+    form.setAttribute("action", `/question/:${roomId}/${questionId}/${slug}`)
 
     modalTitle.innerHTML = `${text} esta pergunta?`
     modalDescripition.innerHTML = `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta?`
--- src/route.js
+++ src/route.js
@@ -1,13 +1,17 @@
 const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render("index"))
-route.get('/room', (req, res) => res.render("room"))
-route.get('/create-room', (req, res) => res.render("create-room"))
+route.get('/', (req, res) => res.render("index", {page:'enter-room'}))
+route.get('/create-room', (req, res) => res.render("index", {page:'create-room'}))
+
+route.get('/room/:roomId', (req, res) => res.render("room"))
 
 //formto do formulario da modal
-//route.get('/room/:room/:question/:action')
+route.post('/question/:room/:question/:action', QuestionController.index)
+route.post('/my-room', RoomController.create)
 
 
 
